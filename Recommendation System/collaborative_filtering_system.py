7ddcde0350db7251e45fd2473b86ea5520afa55f
QiuBingCheng <bingchengqiu@protonmail.com>
2022-12-17 16:18:54 +0800

    Update collaborative_filtering_system.py

--- Recommendation System/collaborative_filtering_system.py
+++ Recommendation System/collaborative_filtering_system.py
@@ -49,6 +49,9 @@ def find_common_movies(user1,user2):
 def recommend(user, num=10, top_n=15):
     """Recommend n movies to the user
     
+    We find the top_n movies of most high average ratings
+    by the most similar users.
+    
     Parameters
     ----------
     user : int
@@ -60,12 +63,11 @@ def recommend(user, num=10, top_n=15):
         
     Returns
     -------
-    list
-        a list of integer representing the movie ids recommended
+    top_ratings
+        a dataframe includes the most recommended movies and average ratings.
     """
    
-    
-    #calculate the similarity between the user and other users
+    # Calculate the similarity between the user and other users
     similarities = []
     user_ids = []
     for other_user in df.userId.unique():
@@ -83,19 +85,20 @@ def recommend(user, num=10, top_n=15):
     most_similar_users = user_ids[sorted_index] 
     print(f"Top-{num} similar users: {most_similar_users}")
     
-    # Find the movie the user haven't seen
+    # Find the movies the user hasn't seen and the similar users have seen.
     seen_movies = np.unique(df.loc[df["userId"]==user,"movieId"].values)
     not_seen_cond = df["movieId"].isin(seen_movies)==False
     similar_cond = df["userId"].isin(most_similar_users)
     not_seen_movies_ratings = df[not_seen_cond & similar_cond][["movieId","rating"]]
     
     # Find average ratings by the most similar users
-    average_ratings = not_seen_movies_ratings[similar_cond].groupby("movieId").mean()
+    average_ratings = not_seen_movies_ratings.groupby("movieId").mean()
     average_ratings.reset_index(inplace=True)
     top_ratings = average_ratings.sort_values(by="rating",ascending=False).iloc[:top_n]
+    top_ratings.reset_index(inplace=True,drop=True)
     print(f"Top-{top_n} average ratings by the most simiilar users:")
     print(top_ratings.to_string(index=False))
+    
+    return most_similar_users, top_ratings
 #%%
-top_ratings = recommend(user_id=1, num=10, top_n=15)
-    
-    
+most_similar_users, top_ratings = recommend(user=1, num=10, top_n=15)
